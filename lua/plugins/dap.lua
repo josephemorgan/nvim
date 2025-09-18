f3745b0b1e36b17a92dc22eebf0cc590c8399700
Joseph Morgan <jm@jemorgan.dev>
2025-09-17 19:16:55 -0700

    Removing depricated debug adapter

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,94 +3,9 @@ return {
 	dependencies = {
 		"mxsdev/nvim-dap-vscode-js",
 		{
-			"microsoft/vscode-js-debug",
-			build = "npm ci --loglevel=error && npx gulp vsDebugServerBundle && mv dist out",
-		},
-		{
 			"Joakker/lua-json5",
 			build = "./install.sh",
 		},
-		-- {
-		-- 	"rcarriga/nvim-dap-ui",
-		-- 	dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-		-- 	opts = {
-		-- 		controls = {
-		-- 			element = "repl",
-		-- 			enabled = true,
-		-- 			icons = {
-		-- 				disconnect = "",
-		-- 				pause = "",
-		-- 				play = "",
-		-- 				run_last = "",
-		-- 				step_back = "",
-		-- 				step_into = "",
-		-- 				step_out = "",
-		-- 				step_over = "",
-		-- 				terminate = "",
-		-- 			},
-		-- 		},
-		-- 		element_mappings = {},
-		-- 		expand_lines = true,
-		-- 		floating = {
-		-- 			border = "single",
-		-- 			mappings = {
-		-- 				close = { "q", "<Esc>" },
-		-- 			},
-		-- 		},
-		-- 		force_buffers = true,
-		-- 		icons = {
-		-- 			collapsed = "",
-		-- 			current_frame = "",
-		-- 			expanded = "",
-		-- 		},
-		-- 		layouts = {
-		-- 			{
-		-- 				elements = {
-		-- 					{
-		-- 						id = "scopes",
-		-- 						size = 0.25,
-		-- 					},
-		-- 					{
-		-- 						id = "breakpoints",
-		-- 						size = 0.25,
-		-- 					},
-		-- 					{
-		-- 						id = "stacks",
-		-- 						size = 0.25,
-		-- 					},
-		-- 				},
-		-- 				position = "left",
-		-- 				size = 40,
-		-- 			},
-		-- 			{
-		-- 				elements = {
-		-- 					{
-		-- 						id = "repl",
-		-- 						size = 0.5,
-		-- 					},
-		-- 					{
-		-- 						id = "console",
-		-- 						size = 0.5,
-		-- 					},
-		-- 				},
-		-- 				position = "bottom",
-		-- 				size = 10,
-		-- 			},
-		-- 		},
-		-- 		mappings = {
-		-- 			edit = "e",
-		-- 			expand = { "<CR>", "<2-LeftMouse>" },
-		-- 			open = "o",
-		-- 			remove = "d",
-		-- 			repl = "r",
-		-- 			toggle = "t",
-		-- 		},
-		-- 		render = {
-		-- 			indent = 1,
-		-- 			max_value_lines = 100,
-		-- 		},
-		-- 	},
-		-- },
 	},
 	lazy = false,
 	config = function()
@@ -115,43 +30,6 @@ return {
 				toolArgs = { "-d", "emulator-5554" },
 			},
 		}
-
-		require("dap").adapters["pwa-node"] = {
-			type = "server",
-			host = "localhost",
-			port = "${port}",
-			executable = {
-				command = "node",
-				args = {
-					vim.call("stdpath", "data") .. "/lazy/vscode-js-debug/out/src/vsDebugServer.js",
-					"${port}",
-				},
-			},
-		}
-
-		for _, language in ipairs({ "typescript", "javascript" }) do
-			require("dap").configurations[language] = {
-				{
-					type = "pwa-node",
-					name = "Node",
-					request = "launch",
-					program = "${file}",
-					cwd = "${workspaceFolder}",
-					console = "integratedTerminal",
-					skipFiles = {
-						"<node_internals>/",
-						"${workspaceFolder}/node_modules/",
-					},
-				},
-				{
-					type = "pwa-node",
-					request = "attach",
-					name = "Auto Attach",
-					cwd = vim.fn.getcwd(),
-					protocol = "inspector",
-				},
-			}
-		end
 
 		dap.adapters.coreclr = {
 			type = "executable",
@@ -178,23 +56,5 @@ return {
 				processId = require("dap.utils").pick_process,
 			},
 		}
-
-		-- local dapui = require("dapui")
-		--
-		-- dap.listeners.before.attach.dapui_config = function()
-		-- 	dapui.open()
-		-- end
-		--
-		-- dap.listeners.before.launch.dapui_config = function()
-		-- 	dapui.open()
-		-- end
-		--
-		-- dap.listeners.before.event_terminated.dapui_config = function()
-		-- 	dapui.close()
-		-- end
-		--
-		-- dap.listeners.before.event_exited.dapui_config = function()
-		-- 	dapui.close()
-		-- end
 	end,
 }
