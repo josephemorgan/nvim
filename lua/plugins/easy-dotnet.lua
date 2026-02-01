9d9827bdf1812e3a60aac94ee38f538d97799810
Joseph Morgan <jm@jemorgan.dev>
2026-02-01 11:14:10 -0800

    feat(plugins): load easy-dotnet.nvim only if dotnet is available
    Add a conditional check to load the easy-dotnet.nvim plugin only when the 'dotnet' executable is present in the system PATH.

--- lua/plugins/easy-dotnet.lua
+++ lua/plugins/easy-dotnet.lua
@@ -1,6 +1,9 @@
 return {
 	"GustavEikaas/easy-dotnet.nvim",
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+	cond = function()
+		return vim.fn.executable("dotnet") == 1
+	end,
 	opts = {
 		debugger = {
 			--name if netcoredbg is in PATH or full path like 'C:\Users\gusta\AppData\Local\nvim-data\mason\bin\netcoredbg.cmd'
